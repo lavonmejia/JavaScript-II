321958d8bc9a7503fbf1b406050be312affec483
LaVon Mejia <lavonmejia@gmail.com>
2019-06-11 19:46:01 -0400

    still updating

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,33 +56,21 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach( element => fullName.push(element.first_name + " " + element.last_name)) ;
 console.log(fullName);
-
-
-runners.forEach(function(element) {
-  if runners.id >= 0;
-  runners.first_name, runners.last_name.join(" ")
-  
-  
 
 
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
+let allCaps = runners.map(x => x.first_name.toUpperCase());
 console.log(allCaps); 
 
- return runners.map (function(element);
-    
-  
-  
-  
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+
+let largeShirts = runners.filter(x => x.shirt_size = "L");
 console.log(largeShirts);
-  
- return runners.filter(runners.shirt_size = "L");
   
 
 // ==== Challenge 4: Use .reduce() ====
@@ -96,7 +84,19 @@ console.log(ticketPriceTotal);
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+  //tell each person their t-shirt size
+  forEach
+  
+  let shirtSize = [];
+runners.forEach(function(element) {
+  if runners.id >= 0;
+  fullName.push(element.first_name, element.last_name.join(" "));
+}
+  
+console.log(fullName);
+"Hi Joe you are getting a size ${shirt_size} t-shirt"
 
 // Problem 2
-
+//find people who donated more than $200
 // Problem 3
+  //find people who donated less than the average amount
